94bd1aea262f1bc5cf24ada38c6d2046cd2f29b5
CharuniKaushalya <snkaushi@gmail.com>
2015-10-18 16:11:24 +0530

    Sever Created

--- socket/Program.cs
+++ socket/Program.cs
@@ -10,12 +10,29 @@ namespace socket
 {
     class Program
     {
-        public byte[] Buffer { get; set; }
+        static byte[] Buffer { get; set; }
         static Socket sck;
         static Socket sooock;
         static void Main(string[] args)
         {
             sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(0, 1234));
+            sck.Listen(100);
+
+            Socket accept = sck.Accept();
+            Buffer = new byte[accept.SendBufferSize];
+            int bytesRead = accept.Receive(Buffer);
+            byte[] formatted = new byte[bytesRead];
+            for(int i=0;i<bytesRead ;i++){
+                formatted[i] = Buffer[i];
+            }
+
+            string strData = Encoding.ASCII.GetString(formatted);
+            Console.Write(strData + "\r\n");
+            Console.Read();
+            sck.Close();
+            accept.Close();
+
 
         }
     }
