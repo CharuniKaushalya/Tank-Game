e084402d570f0261ec7648ac70c67a46e9dff501
CharuniKaushalya <snkaushi@gmail.com>
2015-10-25 15:58:26 +0530

    Connected To sever

---
+++ Client/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+     
+    class Program
+    {
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),1234);
+            try
+            {
+                sck.Connect(localEndPoint);
+            }catch{
+                Console.Write("Unable to connnect thhe remote end point\r\n");
+            }
+            Console.Write("Enter Text: ");
+            string str = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(str);
+            sck.Send(data);
+            Console.Write("Data sent\r\n");
+            Console.Write("Press any key to continue.........");
+            Console.Read();
+            sck.Close();
+
+        }
+    }
+}
+
+
--- socket/Program.cs
+++ socket/Program.cs
@@ -12,26 +12,133 @@ namespace socket
     {
         static byte[] Buffer { get; set; }
         static Socket sck;
-        static Socket sooock;
         static void Main(string[] args)
         {
-            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            sck.Bind(new IPEndPoint(0, 1234));
-            sck.Listen(100);
+            ConnectToServer();
 
-            Socket accept = sck.Accept();
-            Buffer = new byte[accept.SendBufferSize];
-            int bytesRead = accept.Receive(Buffer);
-            byte[] formatted = new byte[bytesRead];
-            for(int i=0;i<bytesRead ;i++){
-                formatted[i] = Buffer[i];
+        }
+        public static void ConnectToServer(String val = "JOIN#")
+        {
+            System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+
+
+
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 6000);
+
+            try
+            {
+                if (!clientSocket.Connected)
+                    //clientSocket.Connect(serverIP, port);
+
+                    clientSocket.Connect(localEndPoint);
+
+                NetworkStream serverStream = clientSocket.GetStream();
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(val);
+                serverStream.Write(outStream, 0, outStream.Length);
+
+
+
+                //byte[] inStream = new byte[clientSocket.ReceiveBufferSize];
+                //serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+                //string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+
+                //MessageBox.Show(returndata);
+
+
+                serverStream.Flush();
+                serverStream.Close();
+
+                //var count = serverStream.Read(inStream, 0, inStream.Length);
+                //string returndata = Encoding.ASCII.GetString(inStream, 0, count);
+
+                ListenToServer();
+
+            }
+            catch (Exception m)
+            {
+
+                Console.WriteLine(m.Message);
             }
 
-            string strData = Encoding.ASCII.GetString(formatted);
-            Console.Write(strData + "\r\n");
-            Console.Read();
-            sck.Close();
-            accept.Close();
+            Console.ReadKey();
+
+
+
+        }
+
+
+
+        private static void ListenToServer()
+        {
+            const int PORT_NO = 7000;
+            const string SERVER_IP = "127.0.0.1";
+
+
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+
+            listener.Start();
+
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //---convert the data received into a string---
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+
+
+
+            //Read user commands
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+
+                Console.WriteLine("Sending back : " + "UP#");
+                ConnectToServer("UP#");
+            }
+
+            if (key.Key == ConsoleKey.DownArrow)
+            {
+
+                Console.WriteLine("Sending back : " + "DOWN#");
+                ConnectToServer("UP#");
+            }
+
+
+            if (key.Key == ConsoleKey.LeftArrow)
+            {
+
+                Console.WriteLine("Sending back : " + "LEFT#");
+                ConnectToServer("UP#");
+            }
+
+
+            if (key.Key == ConsoleKey.RightArrow)
+            {
+
+                Console.WriteLine("Sending back : " + "RIGHT#");
+                ConnectToServer("UP#");
+            }
+
+            if (key.Key == ConsoleKey.Enter)
+            {
+
+                Console.WriteLine("Sending back : " + "SHOOT#");
+                ConnectToServer("UP#");
+            }
+
+
 
 
         }
